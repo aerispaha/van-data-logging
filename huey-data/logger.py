36cd7e2d10c2a3677e5bf639d04a3ab6796fd79f
Adam Erispaha <aerispaha@gmail.com>
2020-06-13 22:29:55 -0400

    add internet check and try catch

--- huey-data/logger.py
+++ huey-data/logger.py
@@ -2,18 +2,39 @@ from influxdb import InfluxDBClient
 import time
 from environmental import get_temp_humidity
 from settings import *
+import logging
+import socket
+
+logging.basicConfig('huey.log')
 
 
-def write_to_db(table, data):
-    pass
+def internet(host="8.8.8.8", port=53, timeout=3):
+    """
+    Host: 8.8.8.8 (google-public-dns-a.google.com)
+    OpenPort: 53/tcp
+    Service: domain (DNS/TCP)
+    """
+    try:
+        socket.setdefaulttimeout(timeout)
+        socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect((host, port))
+        return True
+    except socket.error as ex:
+        print(ex)
+        return False
 
+
+# first wait until we're connected to internet
+while not internet():
+    logging.info('not connected to internet')
 
 client = InfluxDBClient(DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME)
 
 while True:
-
-    # gather data
-    env_data = get_temp_humidity()
-    client.write_points(env_data)
+    try:
+        # gather data
+        env_data = get_temp_humidity()
+        client.write_points(env_data)
+    except BaseException as e:
+        logging.error(e)
 
     time.sleep(LOGGER_TIMESTEP)
